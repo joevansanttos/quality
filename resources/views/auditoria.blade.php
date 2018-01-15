ed72718d7c971a26664b2638dbfd6f0e37b67d15
joevansanttos <joevansanttos@gmail.com>
2018-01-15 12:25:45 -0300

    15/01

--- resources/views/auditoria.blade.php
+++ resources/views/auditoria.blade.php
@@ -10,37 +10,296 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('imagens/favicon.ico') }}"/>
     <link rel="stylesheet" href="{{ URL::asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('vendors/bootstrap/dist/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('css/imprime.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/auditoria.css') }}" />
   </head>
   <body>
-  	<page size="A4">
+  	<page size="A4" class="break">
       <div>
         <img src="{{ URL::asset('imagens/logo.png') }}">
       </div>
-      <br>
-      <br>
   		<div>
-  			<h2>RELATÓRIO DE AUDITORIA</h2>
-
-            <h3>{{$a->cliente->nome}}</h3>
+        <br>
+        <br>
+        <br>
+  			<h1 id="titulo" class="capa">RELATÓRIO DE AUDITORIA</h1>
+        <h2 class="capa">{{$a->cliente->nome}}</h2>
+        <h2 class="capa">{{$a->cod}}</h2>
+        <h2 class="capa">PROJEK</h2>
+        <h2 class="capa">Auditoria Principal/</h2>
   		</div>
   	</page>
+
+    <page size="A4" class="break">
+      <h2 id="title">ÍNDICE</h2>
+      <hr>
+      <br>
+      <p><strong>1. INFORMAÇÕES GERAIS</strong></p>
+      <p>1.1 INFORMAÇÕES DA ORGANIZAÇÃO</p>
+      <p>1.2 INFORMAÇÕES DO CONTRATO</p>
+      <br>
+      <p><strong>2. INFORMAÇÕES DA AUDITORIA</strong></p>
+      <p>2.1 NORMAS DA AUDITORIA</p>
+      <p>2.2 ESCOPO DE CERTIFICAÇÃO</p>
+      <p>2.3 INFORMAÇÕES DO AUDITOR</p>
+      <p>2.4 ESCOPO DE AUDITORIA
+      </p>
+      <br>
+      <p><strong>3. PROCESSO DA AUDITORIA</strong></p>
+      <p>3.1 NOTAS DE AUDITORIA</p>
+      <p>3.2 REVISÃO DOCUMENTAL (AUDITORIA INICIAL)</p>
+      <p>3.3 RELATÓRIO SUMÁRIO POR NORMA</p>
+      <p>3.4 RELATÓRIO DE NÃO CONFORMIDADE</p>
+      <br>
+      <p><strong>4. DESEMPENHO</strong></p>
+      <br>
+      <p><strong>5. SUMÁRIO EXECUTIVO</strong></p>
+      <p>5.1 CONCLUSÕES DA AUDITORIA</p>
+      <p>5.2 SUMÁRIO DAS NÃO-CONFORMIDADES</p>
+      <p>5.3 EFICÁCIA DO SISTEMA DE GESTÃO</p>
+      <p>5.4 OBSERVAÇÕES</p>
+      <br>
+      <p><strong>6. RECOMENDAÇÃO DO AUDITOR LÍDER</strong></p>
+      <br>
+      <p><strong>7. PROGRAMA DE AUDITORIAS</strong></p>
+    </page>
+
     <page size="A4">
-        <hr>
-        <h3 id="title">1. Informações Gerais</h3>
+      <h2 id="title">1. INFORMAÇÕES GERAIS</h2>
+      <hr>
       <br>
       <div>
-        <h4 id="title">1.1 Informações da Organização</h4>
+        <h3 id="title">1.1 INFORMAÇÕES DA ORGANIZAÇÃO</h3>
         <table class="table table-bordered">
+          <tr>
             <td>Organização</td>
+            <td>{{$a->cliente->nome}}</td>
+          </tr>
+          <tr>
             <td>Endereço</td>
+            <td>{{$a->cliente->endereco}}</td>
+          </tr>
+          <tr>
             <td>Cidade</td>
+            <td>{{$a->cliente->cidades->CT_NOME }}</td>
+          </tr>
+          <tr>
             <td>Cep</td>
+            <td>{{$a->cliente->cep}}</td>
+          </tr>
+          <tr>
             <td>Estado</td>
-            <td>País</td>
+            <td>{{$a->cliente->estado}}</td>
+          </tr>
+          <tr>
+            <td>País</td> 
+            <td>Brazil</td>
+          </tr> 
+          <tr>
+            <td>Telefone</td> 
+            <td>{{$a->cliente->tel}}</td>
+          </tr> 
+          <tr>
+            <td>Fax</td> 
+            <td>{{$a->cliente->fax}}</td>
+          </tr> 
+          <tr>
+            <td>Contrato</td> 
+            <td>{{$a->cliente->contrato}}</td>
+          </tr>           
+        </table>
+        <br>
+        <h3 id="title">1.2 INFORMAÇÕES DO CONTATO</h3>
+        <table class="table table-bordered">
+
+          <tr>
+          @foreach ($a->contatos as $contato)
+            <tr>
+              <td>Nome do Contato</td>
+              <td>{{$contato->nome}}</td>
+            </tr>
+            <tr>
+              <td>Email do Contato</td>
+              <td>{{$contato->email}}</td>
+            </tr>
+            <tr>
+              <td>Telefone do Contato</td>
+              <td>{{$contato->telefone}}</td>
+            </tr>
+          @endforeach
+          </tr>
+        </table>
+      </div>
+    </page> 
+
+    <page size="A4">
+      <h2 id="title">2. INFORMAÇÕES DA AUDITORIA</h2>
+      <hr>
+      <br>
+      <div>
+        <h3 id="title">2.1 NORMAS DA AUDITORIA</h3>
+        <table class="table table-bordered">
+          @foreach ($a->normas as $norma)
+          <tr>
+            <td>Normas da Auditoria</td>
+            <td>{{$norma->norma}}</td>
+          </tr>
+          @endforeach
+        </table>
+        <br>
+        <h3 id="title">2.2 ESCOPO DE CERTIFICAÇÃO</h3>
+        <table class="table table-bordered">
+          <tr>
+            <td>Inglês</td>
+            <td>{{$a->certificacao->ingles}}</td>
+          </tr>
+          <tr>
+            <td>Português (Brazil)</td>
+            <td>{{$a->certificacao->portugues}}</td>
+          </tr>
+          <tr>
+            <td>Nº de Sites</td>
+            <td>{{$a->certificacao->sites}}</td>
+          </tr>
+          <tr>
+            <td>Nº de Funcionários</td>
+            <td>{{$a->certificacao->funcionarios}}</td>
+          </tr>
+          <tr>
+            <td>Escritório Central</td>
+            <td>{{$a->certificacao->escritorio}}</td>
+          </tr>
+          <tr>
+            <td>Tipo de Auditoria</td>
+            <td>{{$a->certificacao->tipo}}</td>
+          </tr>
+          <tr>
+            <td>Ínicio</td>
+            <td>{{$a->certificacao->inicio}}</td>
+          </tr>
+          <tr>           
+            <td>Término</td>
+            <td>{{$a->certificacao->termino}}</td>
+          </tr>
+          <tr>           
+            <td>Duração</td>
+            <td>{{$a->certificacao->duracao}}</td>
+          </tr>
+        </table>
+        <br>
+        <h3 id="title">2.3 INFORMAÇÕES DO AUDITOR</h3>
+        <table class="table table-bordered">
+          <tr>
+            <td>Líder da Equipe</td>
+            <td>{{$a->auditorinfo->auditor}}</td>
+          </tr>
+          <tr>
+            <td>Membros da Equipe</td>
+            <td>{{$a->auditorinfo->membros}}</td>
+          </tr>
+        </table>
+      </div>
+    </page> 
+
+    <page size="A4">
+      <br>
+      <h3 id="title">2.4 ESCOPO DA AUDITORIA</h3>
+      <table class="table table-bordered">
+        <tr>
+          <td>Objetivos da Auditoria</td>
+        </tr>
+        @if($a->escopo != null)
+        <tr class="data">
+          <td>
+              {{$a->escopo->objetivos}}
+          </td>
+        </tr>
+        @endif
+      </table>
+      <br>
+      <table class="table table-bordered">
+        <tr>
+          <td style="width: 10%;">Data</td>
+          <td style="width: 8%;">Horário</td>
+          <td>Atividade</td>
+          <td style="width: 15%;">Nome do Site</td>
+          <td>Processo</td>
+          <td>Auditor</td>
+          <td>Cláusula</td>
+        </tr>
+        @foreach ($a->planos as $plano)
+          <tr style="font-size: 7pt;">
+            <td>{{$plano->data}}</td>
+            <td>{{$plano->horario}}</td>
+            <td>{{$plano->atividade}}</td>
+            <td>{{$plano->site}}</td>
+            <td>{{$plano->processo}}</td>
+            <td>{{$plano->auditor}}</td>
+            <td>{{$plano->clausula}}</td
+          </tr>
+        @endforeach
+      </table>
+    </page> 
+
+    <page size="A4">
+      <h2 id="title">3. PROCESSO DA AUDITORIA</h2>
+      <hr>
+      <br>
+      <div>
+        <h3 id="title">3.1 NOTAS DA AUDITORIA</h3>
+        <table class="table table-bordered">
+          @foreach ($a->notas as $nota)
+          <tr>
+            <td>Auditor</td>
+            <td>{{$nota->auditor}}</td>
+          </tr>
+          <tr>
+            <td>Data</td>
+            <td>{{$nota->data}}</td>
+          </tr>
+          <tr>
+            <td>Processo</td>
+            <td>{{$nota->processo}}</td>
+          </tr>
+          <tr>
+            <td>Pessoal Auditado</td>
+            <td>{{$nota->pessoal}}</td>
+          </tr>
+          <tr>
+            <td colspan="2" style="text-align: center;">
+              Notas
+            </td>
+          </tr>
+          <tr class="data">
+            <td colspan="2">
+              {{$nota->notas}}
+            </td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
+      <div>
+        <h3 id="title">3.2 REVISÃO DOCUMENTAL (AUDITORIA INICIAL)</h3>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+            <th>Item de Revisão Documental</th>
+            <th>Verificado</th>
+            <th>Comentários</th>
+          </tr>
+          </thead>
+          <tbody>
+            @foreach ($a->revisoes as $revisao)
+              <tr>
+                <td>{{$revisao->item}}</td>
+                <td>x</td>
+                <td>{{$revisao->comentarios}}</td>
+              </tr>
+            @endforeach
+          </tbody>
         </table>
       </div>
     </page>
+
     <script type="text/javascript" src="{{URL::asset('vendors/jquery/dist/jquery.min.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('vendors/Chart.js/dist/Chart.min.js')}}"></script>
